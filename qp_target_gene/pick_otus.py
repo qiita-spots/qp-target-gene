f999f6956399ea8f7191b4e351124be26e0247bc
Antonio Gonzalez <antgonza@gmail.com>
2020-05-05 19:13:02 -0600

    pigz -d

--- qp_target_gene/pick_otus.py
+++ qp_target_gene/pick_otus.py
@@ -73,7 +73,7 @@ def generate_pick_closed_reference_otus_cmd(filepaths, out_dir, parameters,
                 f.read(1)
         except (OSError, IOError):
             seqs_fp_fna = join(out_dir, 'seqs.fna')
-            cmd_ungz = 'pigz -c -p%s %s > %s && ' % (
+            cmd_ungz = 'pigz -c -d -p%s %s > %s && ' % (
                 seqs_fp, parameters['threads'], seqs_fp_fna)
             seqs_fp = seqs_fp_fna
 
